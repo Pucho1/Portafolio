324055dfaea5f9de18d8148e0e6fb59c0ca2e34f
Pucho <martinezochandarenam@gmail.com>
2025-07-13 17:52:16 +0200

    style: slow velocity of animation menu mobile

--- src/components/mobileMenu/Mobilemenu.tsx
+++ src/components/mobileMenu/Mobilemenu.tsx
@@ -11,41 +11,46 @@ const MobileMenu = ({ isOpen, navLinks }: MobileMenuProps) => {
       <>
         {/* Menu */}
         <div
-          className={`fixed top-0 right-0 h-dvh w-full md:w-1/2 lg:w-1/3 bg-[#141517] z-50 transform transition-transform duration-300 ease-in-out ${
+          className={`fixed top-0 right-0 h-dvh w-full md:w-1/2 lg:w-1/3 bg-[#141517] z-50 transform transition-transform duration-500 ease-in-out ${
             isOpen ? "translate-x-0" : "translate-x-full"
-          }`}
+          } delay-80`}
         >
-          {/* Header */}
-          <div className="flex flex-col  p-6 pb-0 h-[21%]">
-            <div className="flex w-full ustify-start h-full">
-              <h5 className="flex items-end text-white mb-6 font-bold uppercase text-[0.6em] opacity-50">
-                navigation
-              </h5>
-            </div>
+          <div className={`h-full retard bg-trasparent transform transition-transform duration-700 ease-in-out ${
+            isOpen ? "translate-x-0" : "translate-x-full"} delay-150`}>
 
-            <hr className="text-gray-200" />
-          </div>
-
-          {/* Content */}
-          <div className="flex-1 p-6 pb-0 h-[63%]">
-            {/* Section 1: Navigation */}
-            <div className="h-full">
-              <div className="space-y-2">
-
-                {navLinks.map((link, index) => (
-                  <MobileBtn name={link.name}  href={link.href} key={index}/>
-                ))}
-
+            {/* Header */}
+            <div className="flex flex-col  p-6 pb-0 h-[21%]">
+              <div className="flex w-full ustify-start h-full">
+                <h5 className="flex items-end text-white mb-6 font-bold uppercase text-[0.6em] opacity-50">
+                  navigation
+                </h5>
               </div>
+
+              <hr className="text-gray-200" />
             </div>
 
-            <hr className="text-gray-200" />
-          </div>
+            {/* Content */}
+            <div className="flex-1 p-6 pb-0 h-[63%]">
+              {/* Section 1: Navigation */}
+              <div className="h-full">
+                <div className="space-y-2">
 
-          {/* footer  */}
-          <footer>
-            <TimeLinksFooter showLocalTime={ false}/>
-          </footer>
+                  {navLinks.map((link, index) => (
+                    <MobileBtn name={link.name}  href={link.href} key={index}/>
+                  ))}
+
+                </div>
+              </div>
+
+              <hr className="text-gray-200" />
+            </div>
+
+            {/* footer  */}
+            <footer>
+              <TimeLinksFooter showLocalTime={ false}/>
+            </footer>
+
+          </div>
         </div>
       </>,
       document.body,
